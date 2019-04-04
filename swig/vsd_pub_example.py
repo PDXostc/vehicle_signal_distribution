bb67ffcca7ea831c17c15acadb26bdb983b94943
Magnus Feuer <mfeuer1@jaguarlandrover.com>
2019-04-04 10:30:13 -0700

    Now works

--- swig/vsd_pub_example.py
+++ swig/vsd_pub_example.py
@@ -7,8 +7,6 @@ import vsd
 if __name__ == "__main__":
     ctx = vsd.create_context()
     vsd.load_from_file(ctx, "../vss_rel_2.0.0-alpha+005.csv")
-
-
     vsd.process_events(300000)
 
     sig = vsd.signal(ctx, "Vehicle.Drivetrain.Transmission.Gear")
@@ -16,5 +14,4 @@ if __name__ == "__main__":
 
     pub = vsd.signal(ctx, "Vehicle.Drivetrain.Transmission")
     vsd.publish(pub);
-    print("Should be 4: {}".format(vsd.get(ctx, sig)))
     vsd.process_events(300000)
--- swig/vsd_sub_example.py
+++ swig/vsd_sub_example.py
@@ -2,12 +2,12 @@
 import vsd
 ctx = None
 def process_signal(signal, path, value):
-    print("YES {}:{} - {}".format(signal, path, value))
+    print("Sub {}:{} - {}".format(signal, path, value))
 
 if __name__ == "__main__":
     ctx = vsd.create_context()
     vsd.set_callback(ctx, process_signal)
     vsd.load_from_file(ctx, "../vss_rel_2.0.0-alpha+005.csv")
-    sig = vsd.signal(ctx, "Vehicle.Drivetrain");
+    sig = vsd.signal(ctx, "Vehicle.Drivetrain.Transmission");
     vsd.subscribe(ctx, sig)
     vsd.process_events(-1)
